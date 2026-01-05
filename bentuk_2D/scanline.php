13770732b6f04121acf6c5611bdc3b43263145a2
muhammadilhamcode <muhammadilhamabdulgani@gmail.com>
2026-01-05 19:16:10 +0800

    Update scanline.php

--- bentuk_2D/scanline.php
+++ bentuk_2D/scanline.php
@@ -1,2 +1,17 @@
 <?php
+$x = 0;
+$y = 0;
+
+function scanline($vx, $vy){
+}
+
+do{
+    $x++;
+    if(($x==$x_max)&&($y<$y_max)){
+       $x = 0;
+    }
+    $y++;
+    scanline($x, $y);
+}while(($x<$x_max)&&($y<$y_max));
+
 ?>
