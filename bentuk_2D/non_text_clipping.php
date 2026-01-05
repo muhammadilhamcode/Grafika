62c7124c4896a784f6b62fe63c18b8796f70a786
muhammadilhamcode <muhammadilhamabdulgani@gmail.com>
2026-01-06 05:43:58 +0800

    Update non_text_clipping.php

--- bentuk_2D/non_text_clipping.php
+++ bentuk_2D/non_text_clipping.php
@@ -1,6 +1,6 @@
 <?php
 // clipping area
 $clipping_area = array(
-"p1" => array("x" => 50), "p2" => array(), "p3" => array(),"p4" => array()
+"p1" => array("x" => 50, "y" => 50), "p2" => array(), "p3" => array(),"p4" => array()
 );
 ?>
